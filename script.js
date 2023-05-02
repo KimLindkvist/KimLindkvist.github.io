8b31179b6cf9ce0d53cab4a7d6432b23b7094e9e
Kim Lindkvist <kim.lindkvist@cgi.com>
2023-05-02 14:18:24 +0200

    first push

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+  window.onscroll = function () {
+    const backToTopButton = document.getElementById('backToTop');
+  
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  };
+  
